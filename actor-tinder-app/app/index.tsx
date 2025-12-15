fe7ffec459f9d8dcc3badbe1dc4aaf6ec32b9efb
N.Z. Taylor <n.z.taylor@umail.leidenuniv.nl>
2025-12-15 21:33:52 +0100

    Edit index.tsx

--- actor-tinder-app/app/index.tsx
+++ actor-tinder-app/app/index.tsx
@@ -5,7 +5,7 @@ import { Button, Dimensions, Image, ScrollView, StyleSheet, Text, View } from "r
 import Swiper from "react-native-deck-swiper";
 
 const { width, height } = Dimensions.get("window");
-const API = "http://145.118.236.114:8000"; // replace with your backend IP
+const API = "http://PUT YOUR IP HERE:8000"; // replace with your backend IP
 const TMDB_API_KEY = "118d6406df6cc4311fb96f3c4e44f65c";
 
 interface Actor {
