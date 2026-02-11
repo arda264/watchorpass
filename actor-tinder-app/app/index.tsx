7d8a676bf1e8d81867e01e181389653a4d1cd36d
A. Çavușoğlu <a.cavusoglu@umail.leidenuniv.nl>
2026-02-11 18:32:49 +0300

    trying to make frontend prettier.

--- actor-tinder-app/app/index.tsx
+++ actor-tinder-app/app/index.tsx
@@ -508,7 +508,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-evenly',
     paddingHorizontal: 40,
     paddingBottom: 10,
-    paddingTop: 40,
+    paddingTop: 90,
   },
   actionButton: {
     width: 70,
